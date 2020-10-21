5012da1ca32f8dc3daa7ba0bf92f010270aebca4
renato76 <renmignogna@gmail.com>
2020-10-21 18:40:34 +0100

    added MovieListView to view.py in movies folder

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import response
 
-# Create your views here.
+from .models import Movie
+from .serializers.common import MovieSerializer
+
+class MoviesListView(APIView):
+
+    def get(self,request):
+        movies_list = Movie.objects.all()
+        serialized_movies_list = MovieSerializer(movies_list, many=True)
+        return Response(serialized_movies_list.data, status=status.HTTP_200_OK)
+
