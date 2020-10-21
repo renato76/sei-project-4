532c8b6505eb7b93169ad4dd6da563bda1d4e9b0
renato76 <renmignogna@gmail.com>
2020-10-21 18:58:18 +0100

    added MovieListView and its now working in Insomnia I can fetch all movies

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
-from rest_framework.response import response
+from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Movie
 from .serializers.common import MovieSerializer
