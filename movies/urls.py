f2dae217a9b42be3faa6e4ee7fdded13cc70662d
renato76 <renmignogna@gmail.com>
2020-10-22 10:38:55 +0100

    added MovieDetailView in views.py show one movie all working fine in insomnia

--- movies/urls.py
+++ movies/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MoviesListView
+from .views import MoviesListView, MovieDetailView
 
 urlpatterns = [
-    path('', MoviesListView.as_view())
+    path('', MoviesListView.as_view()),
+    path('<int:pk>/', MovieDetailView.as_view())
 ]
--- movies/views.py
+++ movies/views.py
@@ -7,11 +7,14 @@ from .serializers.common import MovieSerializer
 
 class MoviesListView(APIView):
 
-    def get(self,request):
+    def get(self, _request):
         movies_list = Movie.objects.all()
         serialized_movies_list = MovieSerializer(movies_list, many=True)
         return Response(serialized_movies_list.data, status=status.HTTP_200_OK)
 
-# class MovieDetailView(APIView):
-    
-
+class MovieDetailView(APIView):
+    def get(self, _request, pk):
+        movie = Movie.objects.get(pk=pk)
+        serialized_movie = MovieSerializer(movie)
+        return Response(serialized_movie.data, status=status.HTTP_200_OK)
+        
