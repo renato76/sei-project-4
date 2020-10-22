e2d954d2b8cd54a8997523723e335d732233f974
renato76 <renmignogna@gmail.com>
2020-10-22 16:27:28 +0100

    token now works and allows user to create a movie when logged in

--- jwt_auth/authentication.py
+++ jwt_auth/authentication.py
@@ -2,7 +2,7 @@ from rest_framework.authentication import BasicAuthentication
 from rest_framework.exceptions import PermissionDenied
 from django.contrib.auth import get_user_model
 from django.conf import settings
-import jwt_auth
+import jwt
 
 User = get_user_model()
 
